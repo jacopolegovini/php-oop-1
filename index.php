c14801d5d7cabca9bd2851d5f93b79aeb0943102
jacopolegovini <legovini96@gmail.com>
2024-10-09 15:10:08 +0200

    class implemented

--- index.php
+++ index.php
@@ -1,3 +1,18 @@
 <?php
 
-class Movie {}
+class Movie
+{
+    public $title;
+    public $genre;
+    public $filmLength;
+
+    function __construct($title, $genre, $filmLength)
+    {
+        $this->title = $title;
+        $this->genre = $genre;
+        $this->filmLength = $filmLength;
+    }
+}
+
+$matrix = new Movie('Matrix', 'Action', 136);
+var_dump($matrix);
