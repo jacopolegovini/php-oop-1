1992ead18ce5797b358bcc64959e60ca2df3697b
jacopolegovini <legovini96@gmail.com>
2024-10-09 15:18:41 +0200

    second movie added

--- index.php
+++ index.php
@@ -15,7 +15,7 @@ class Movie
 
     function isBoring($filmLength)
     {
-        if ($filmLength > 120) {
+        if ($filmLength > 130) {
             return true;
         } else {
             return false;
@@ -26,3 +26,7 @@ class Movie
 $matrix = new Movie('Matrix', 'Action', 136);
 var_dump($matrix);
 var_dump($matrix->isBoring($matrix->filmLength));
+
+$inception = new Movie('Inception', 'Sci-fi', 128);
+var_dump($inception);
+var_dump($inception->isBoring($inception->filmLength));
